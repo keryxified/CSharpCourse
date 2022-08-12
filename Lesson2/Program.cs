ecfa140c6c1d2f170dc67b66bcc9d107477f46e2
Kirill Golovin <keryxified@gmail.com>
2022-08-12 19:53:53 +0300

    Add Lesson 2, renamed

---
+++ Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Please, enter your name:");
+            string name;
+            do
+            {
+                name = Console.ReadLine();
+
+            }
+            while (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+            {
+                
+            }
+            
+
+
+        }
+    }
+}
