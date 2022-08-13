7339f03b6e3885a2075160622ed97ebe5133148f
Kirill Golovin <keryxified@gmail.com>
2022-08-13 21:29:46 +0300

    Created exercise to Lesson02

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -7,18 +7,38 @@ namespace Lesson2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
-            Console.WriteLine("Please, enter your name:");
-            string name;
-            do
-            {
-                name = Console.ReadLine();
-
-            }
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
             while (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
             {
-                
+                Console.WriteLine("Please enter correct name!");
+                name = Console.ReadLine();
             }
-            
+            Console.WriteLine("Please enter your surname:");
+            string surname = Console.ReadLine();
+            while (!Regex.IsMatch(surname, @"^[a-zA-Z]+$"))
+            {
+                Console.WriteLine("Please enter correct surname!");
+                surname = Console.ReadLine();
+            }
+            Console.WriteLine("Please enter your age:");
+            string ageinput = Console.ReadLine();
+            int age = 0;
+            while (!int.TryParse(ageinput, out age))
+            {
+                Console.WriteLine("Please, enter the valid age. It should be a number.");
+                ageinput = Console.ReadLine();
+            }
+            Console.WriteLine("Please enter your hobby:");
+            string hobby = Console.ReadLine();
+            while (!Regex.IsMatch(hobby, @"^[a-zA-Z]+$"))
+            {
+                Console.WriteLine("Please enter correct hobby!");
+                surname = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Name: {name}\nSurname: {surname}\nAge: {age}\nHobby: {hobby}");
+
 
 
         }
