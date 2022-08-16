f1920f45ddf234434fb6f0e8bf347ed755e1e40e
Kirill Golovin <keryxified@gmail.com>
2022-08-16 17:19:14 +0300

    Lesson 4 Hometask

---
+++ Lesson4/CachedValue.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson_4
+{
+    internal record CachedValue<T>
+    {
+        public T Value;
+        public DateTime CreationTime;
+        public int Timeout;
+    }
+}
---
+++ Lesson4/SimpleGenericCache.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson_4
+{
+    internal class SimpleGenericCache<T>
+    {
+        private readonly Dictionary<string, CachedValue<T>> _cache = new();
+
+        internal void Store(string key, T value, int timeout = 30)
+        {
+            var cachedValue = new CachedValue<T> { Value = value, CreationTime = DateTime.Now, Timeout = timeout };
+            _cache[key] = cachedValue;
+
+        }
+
+        internal T? Fetch(string key)
+        {
+            if (_cache.TryGetValue(key, out var cachedValue))
+            {
+                DateTime result = cachedValue.CreationTime.AddSeconds(cachedValue.Timeout);
+                if(result >= DateTime.Now)
+                {
+                    return cachedValue.Value;
+                }
+            }
+            return default;
+        }
+    }
+}
